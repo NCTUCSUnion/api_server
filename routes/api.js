4f2d401dd229546f10c424301795505490b781e5
maxam2017 <whk0313@gmail.com>
2018-12-10 21:46:26 +0800

    fix

--- routes/api.js
+++ routes/api.js
@@ -55,4 +55,101 @@ router.post('/pay',function(req,res,next){
 //   )
 // })
 
+router.post('/merryweek/load',function(req,res,next){
+  cs_pool.getConnection(function(err,connection){
+      connection.query(sql.merryweekCheck,[req.body.id],function(err,result){
+        if(err){
+          console.error(err)
+        }
+        else{
+          if(result.length === 0){
+		  var paid;
+		connection.query(sql.checkPay,[req.body.id],function(err,result){
+			console.log(result)
+			if(err) 
+				console.error(err)
+			else{
+				if(result.length ===0){
+					paid = 0;	
+				}
+				else{
+					paid = result[0].paid;
+				}
+			}
+            connection.query(sql.merryweekNew,[req.body.id],function(err,result){
+              console.log(result)
+                if(err){
+                  console.error(err)
+                }
+                else{
+                  res.json({
+                    code: `${paid}${paid}0000000`
+                  })
+                }
+            })
+          })}
+          else{
+            res.json({
+              code: "".concat(
+                result[0].paid,
+                result[0].paid,
+                result[0].shootingStar,
+                result[0].gingerbread,
+                result[0].pokemonGo,
+                result[0].balloonWall,
+                result[0].hunterxhunter,
+                result[0].partyTicket,
+                result[0].partyTicket)
+            })
+          }
+        }
+        connection.release()
+      })
+    }
+  )
+})
+
+router.post('/merryweek/update',function(req,res,next){
+  cs_pool.getConnection(function(err,connection){
+    var game = ["shootingStar","gingerbread","pokemonGo","balloonWall","hunterxhunter","partyTicket"][req.body.code]
+    var sql = `UPDATE xmasweek SET ${game[req.body.game]} = 1 WHERE xmasweek.id = ${req.body.id}`
+    connection.query(sql,function(err,result){
+      if(err){
+        console.error(err)
+      }
+      else{
+        res.json({success:(result.changedRows === 1)})
+      }
+      connection.release()
+    })
+  })
+})
+
+router.post('/merryweek/upload',function(req,res,next){
+  cs_pool.getConnection(function(err,connection){
+    connection.query(sql.merryweekUpload,[
+      req.body.id, req.body.url, req.body.title, req.body.description
+    ],function(err,result){
+      if(err){
+        console.error(err)
+      }
+      else{
+        if(first){
+          connection.query(`UPDATE xmasweek SET shootingStar = 1 WHERE xmasweek.id = ${req.body.id}`
+          ,function(err,result){
+            if(err){
+              console.error(err)
+            }
+            else{
+              res.json({success:(result.changedRows === 1)})
+            }
+          })
+        }
+        res.json({success:(result.changedRows === 1)})
+      }
+      connection.release()
+    })
+  })
+})
+
 module.exports = router;
